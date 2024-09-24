3ea6f0cf9369174e03c81db78ac11cbe8991297c
Diego Paccotaype <diego200997@gmail.com>
2024-09-24 00:24:29 -0500

    Creacion del proyecto

---
+++ src/main/java/org/diegopacco/corfid/CorfidApplication.java
@@ -0,0 +1,13 @@
+package org.diegopacco.corfid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorfidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorfidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/diegopacco/corfid/CorfidApplicationTests.java
@@ -0,0 +1,13 @@
+package org.diegopacco.corfid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorfidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
